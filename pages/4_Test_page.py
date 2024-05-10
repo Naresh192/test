414a089ddcaf0d79a87e2ad684f5f66679263a02
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2024-05-10 15:05:18 +0530

    Create 4_Test_page.py

---
+++ pages/4_Test_page.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle 
+try :
+    video = open('video.pkl', 'rb')
+except :
+    video = open('video.pkl', 'wb')
+    pickle.dump('', video)
+    video.close()
+    video = open('video.pkl', 'rb')
+try :
+    chat = open('chat.pkl', 'rb')
+except :
+    chat = open('chat.pkl', 'wb')
+    pickle.dump([], chat)
+    chat.close()
+    chat = open('chat.pkl', 'rb')
+l=pickle.load(video)
+video_url=st.text_input("URL",'')
+if video_url!='' and video_url!='None':
+    video_player = st.video(video_url,autoplay=True)
+    st.write(dir(video_player))
+elif l!='' and l!='None' :
+    video_player = st.video(l)
+else :
+    pass
+chat=pickle.load(chat)
+message=st.text_input('Text','')
+if st.button("Send") :
+    chat.append(message)
+for i in chat :
+    st.write(i)
+if st.button("Clear Chat") :
+    chat=[]
+
+video = open('video.pkl', 'wb')
+pickle.dump(video_url, video)
