b40ce1516f1d4b135a784b1e64912a0615a420d7
Naresh192 <35963618+Naresh192@users.noreply.github.com>
2024-05-10 19:55:05 +0530

    Update 4_Test_page.py

--- pages/4_Test_page.py
+++ pages/4_Test_page.py
@@ -31,21 +31,22 @@ else :
     pass
 chat=pickle.load(chat)
 message=st.text_input('Text','')
-if st.button("Send") :
+c1,c2,c3=st.columns(3)
+if c1.button("Send") :
     chat.append(message)
 a=st.empty()
 chats=''
 for i in chat[::-1] :
     chats+='\n'+i
 a.text(chats)
-if st.button("Refresh Chat") :
+if c2.button("Refresh Chat") :
     a.empty()
     chats=''
     for i in chat[::-1] :
         chats+='\n'+i
     a.text(chats)
 
-if st.button("Clear Chat") :
+if c3.button("Clear Chat") :
     chat=[]
 if video_url!='None' :
     video = open('video.pkl', 'wb')
